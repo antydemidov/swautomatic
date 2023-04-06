bcbe737ca8e5226c7dacdf1624447fb6b9d28545
vvihorev <vvihorevv@gmail.com>
2023-04-06 09:33:12 +0200

    Add env vars support, add requirements

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 import os
 
+import dotenv
+
+
+dotenv.load_dotenv()
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    HOST = os.environ.get('HOST') or 'you-will-never-guess'
+    PORT = os.environ.get('PORT') or 'you-will-never-guess'
+    MONGO_USERNAME = os.environ.get('MONGO_USERNAME') or 'you-will-never-guess'
+    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD') or 'you-will-never-guess'
